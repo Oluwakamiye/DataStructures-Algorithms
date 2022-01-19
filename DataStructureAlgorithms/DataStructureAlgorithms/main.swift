e4fe3df64587b3ef25d286ebcdb5d9e6f7fd42b1
Kamiye <kamiye.akindele@kudabank.com>
2022-01-19 07:55:07 +0100

    Added Linked List Data Structure with the basic operations and Copy on Write feature implemented

--- DataStructureAlgorithms/DataStructureAlgorithms/main.swift
+++ DataStructureAlgorithms/DataStructureAlgorithms/main.swift
@@ -9,4 +9,36 @@ import Foundation
 
 // Stack Call
 var stack: Stack = [1,2,3,4,5,6]
-print(stack.debugDescription)
+//print(stack.debugDescription)
+
+// LinkedListNode Call
+var list = LinkedList<Int>()
+list.append(3)
+list.append(2)
+list.append(1)
+list.push(4)
+list.append(8)
+list.append(12)
+//print(list)
+//print("Popped item \(list.removeLast()!)")
+//print(list)
+
+
+let node = list.node(at: 5)!
+let removedValue = list.remove(after: node)
+
+/*
+print("removed Value is \(removedValue)")
+print("List after is \(list)")
+ */
+print("List1 unquely referenced? \(isKnownUniquelyReferenced(&list.head))")
+var list2 = list
+//print("List 1 :: \(list)")
+//print("List 2 :: \(list2)")
+//print("After appending")
+//list2.append(19)
+//print("List 1 :: \(list)")
+//print("List 2 :: \(list2)")
+
+
+print("List1 unquely referenced? \(isKnownUniquelyReferenced(&list.head))")
