3d56413ad9fbdc7fbb64ea671c2f83e4369aa0d9
Oluwakamiye Akindele <karmmie1@gmail.com>
2022-03-24 08:22:11 +0000

    Added Insert and Removal Operations to Heap Data Structure

--- DataStructureAlgorithms/DataStructureAlgorithms/Heap/Heap.swift
+++ DataStructureAlgorithms/DataStructureAlgorithms/Heap/Heap.swift
@@ -38,4 +38,55 @@ struct Heap<T: Equatable> {
     func parentIndex(ofChildAt index: Int) -> Int {
         (index - 1) / 2
     }
+    
+    mutating func remove() -> T? {
+        guard !isEmpty else {
+            return nil
+        }
+        elements.swapAt(0, count - 1)
+        defer {
+            siftDown(from: 0)
+        }
+        return elements.removeLast()
+    }
+    
+    mutating func siftDown(from index: Int) {
+        var parent = index
+        
+        while true {
+            let left = leftChildIndex(ofParentAt: parent)
+            let right = rightChildIndex(ofParentAt: parent)
+            var candidate = parent
+            
+            if left < count && sort(elements[left], elements[candidate]) {
+                candidate = left
+            }
+            if right < count && sort(elements[right], elements[candidate]) {
+                candidate = right
+            }
+            if candidate == parent {
+                return
+            }
+            elements.swapAt(parent, candidate)
+            parent = candidate
+        }
+    }
+    
+    mutating func insert(value: T) {
+        elements.append(value)
+        siftUp(from: elements.count - 1)
+    }
+    
+    mutating func siftUp(from index: Int) {
+        guard elements.count > index else {
+            return
+        }
+        var child = index
+        var parent = parentIndex(ofChildAt: child)
+        while child > 0 && sort(elements[child], elements[parent]) {
+            elements.swapAt(child, parent)
+            child = parent
+            parent = parentIndex(ofChildAt: child)
+        }
+    }
 }
